c40fa7cf4755e2c2afdad5c47ae017f315a8f690
Felipe Rosene Klapsztein <feliperosenek@gmail.com>
2022-07-08 22:45:41 -0300

    Create app.ino

---
+++ app.ino
@@ -0,0 +1,78 @@
+#ifdef ENABLE_DEBUG
+   #define DEBUG_ESP_PORT Serial
+   #define NODEBUG_WEBSOCKETS
+   #define NDEBUG
+#endif 
+
+#include <Arduino.h>
+#ifdef ESP8266 
+   #include <ESP8266WiFi.h>
+#endif 
+#ifdef ESP32   
+   #include <WiFi.h>
+#endif
+
+#include "SinricPro.h"
+#include "SinricProSwitch.h"
+
+#define WIFI_SSID         "K"
+#define WIFI_PASS         "baianaelatifa"
+#define APP_KEY           "0a95c7b9-021f-4fa3-b502-c6a2100da412"
+#define APP_SECRET        "b9188afe-14fc-4806-ba59-607bcc7856f2-1059b3c0-c67a-4da6-a7ab-175b8b43fc63"
+
+#define SWITCH_ID_1       "627d9550a6872f363926a32b"
+#define RELAYPIN_1        23
+#define RELAYPIN_2        22
+#define RELAYPIN_3        21
+#define RELAYPIN_4        19
+
+#define BAUD_RATE         115200                // Change baudrate to your need
+
+bool onPowerState1(const String &deviceId, bool &state) {
+ Serial.printf("Device 1 turned %s", state?"on":"off");
+ digitalWrite(RELAYPIN_1, state ? LOW:HIGH);
+ return true; // request handled properly
+}
+
+// setup function for WiFi connection
+void setupWiFi() {
+  Serial.printf("\r\n[Wifi]: Connecting");
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.printf(".");
+    delay(250);
+  }
+
+  Serial.printf("connected!\r\n[WiFi]: IP-Address is %s\r\n", WiFi.localIP().toString().c_str());
+}
+
+// setup function for SinricPro
+void setupSinricPro() {
+  // add devices and callbacks to SinricPro
+  pinMode(RELAYPIN_1, OUTPUT);
+  pinMode(RELAYPIN_2, OUTPUT);
+  pinMode(RELAYPIN_3, OUTPUT);
+  pinMode(RELAYPIN_4, OUTPUT);
+    
+  SinricProSwitch& mySwitch1 = SinricPro[SWITCH_ID_1];
+  mySwitch1.onPowerState(onPowerState1);
+  
+  
+  // setup SinricPro
+  SinricPro.onConnected([](){ Serial.printf("Connected to SinricPro\r\n"); }); 
+  SinricPro.onDisconnected([](){ Serial.printf("Disconnected from SinricPro\r\n"); });
+   
+  SinricPro.begin(APP_KEY, APP_SECRET);
+}
+
+// main setup function
+void setup() {
+  Serial.begin(BAUD_RATE); Serial.printf("\r\n\r\n");
+  setupWiFi();
+  setupSinricPro();
+}
+
+void loop() {
+  SinricPro.handle();
+}
